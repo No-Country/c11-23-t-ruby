dfd963402572feac25473011fce4d02cf4f42b19
YeinerCQ <ycarvajalq@gmail.com>
2023-05-19 20:33:39 -0500

    add account create to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-User.create!(
+user = User.create!(
   email: "user@demo.com",
   password: "123456",
   password_confirmation: "123456"
 )
 
 puts "Demo user has been created."
+
+Account.create!(
+  amount: 0,
+  user: user
+)
+
+puts "user account has been created."
